b01d6670f7ff0a506427f13b7c697beeb6939909
brianramaswami <brianramaswami@yahoo.com>
2018-12-12 14:43:13 -0800

    Create ex3test.c

---
+++ ex3test.c
@@ -0,0 +1,53 @@
+/*
+Class:CPSC 346-02
+Team Member 1: Brian Ramaswami
+Team Member 2: N/A 
+GU Username of project lead: bramaswami
+Pgm Name: proj3.c 
+Pgm Desc: exploraiton of the proc file system 
+Usage: 1) standard:  ./a.out -s 
+Usage: 2) history:  ./a.out -h 
+Usage: 3) load:  ./a.out -l 
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include<time.h>
+#include<math.h>
+
+void standard();
+
+
+int main(int argc, char* argv[])
+{
+ if (argc != 2)
+  {
+   fprintf(stderr, "Error: Options required\n"); 
+   fprintf(stderr, "usage: ./a.out -s|-h|-l\n\n"); 
+   exit(EXIT_FAILURE);
+  }
+  if (!strcmp(argv[1],"-s"))
+   standard();
+}
+/*
+pre: none
+post: displays CPU vendor_id, model name, and OS version
+*/
+void standard()
+{
+ char ch;
+ FILE* ifp;
+ char str[80];
+
+ /*
+ I've deliberately used two different mechanisms for writing to the console.  
+ Use whichever suits you.
+ strstr locates a substring
+ */
+
+ ifp = fopen("/proc/cpuinfo","r");
+ while ((ch = getc(ifp)) != EOF)
+  putchar(ch);
+ fclose (ifp);
+}
